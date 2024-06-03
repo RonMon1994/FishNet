40bc8681aaddb7962eccbf3cd8c6302376b70c0f
RonMon1994 <ronm@mail.tau.ac.il>
2024-06-03 19:15:32 +0300

    new

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from utils import process_data, process_video_frames
 from pathlib import Path
 import torch
 
-processed_data = '2022-0509_med_campaign_stereoBRUVs_ImagePtPair.txt'
-data_3DPoints = pd.read_csv('2022-0509_med_campaign_stereoBRUVs_3DPoints.txt', sep='\t', on_bad_lines='skip')
-data_Lengths = pd.read_csv('2022-0509_med_campaign_stereoBRUVs_Lengths.txt', sep='\t', on_bad_lines='skip')
-output_frames_dir = Path('/home/ronm/Academic/DeepLearning/FinalProject/FishNet-main/Dataset')
+
+import os
+main_path = os.getcwd()
+
+processed_data = os.path.join(main_path, '2022-0509_med_campaign_stereoBRUVs_ImagePtPair.txt')
+data_3DPoints = pd.read_csv(os.path.join(main_path,'2022-0509_med_campaign_stereoBRUVs_3DPoints.txt'), sep='\t', on_bad_lines='skip')
+data_Lengths = pd.read_csv(os.path.join(main_path,'2022-0509_med_campaign_stereoBRUVs_Lengths.txt'), sep='\t', on_bad_lines='skip')
+output_frames_dir = Path(os.path.join(main_path,'Dataset'))
+# This is from the hard disk 
 videos_base_dir = Path('/media/ronm/Crucial X6/chunk_4')
 frame_rate = 60
 
